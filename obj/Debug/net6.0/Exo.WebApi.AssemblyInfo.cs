0118063b9e98c97983b2b15708ab91d1c9c3ff9e
Christian Victor <cvpsdoido24@gmail.com>
2024-06-27 01:15:58 -0300

    first commit

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e0616e1560beafe5ed41ff1dda597682fa80e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93d1315fb83c3216dabbe2702c45335be71f747")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
